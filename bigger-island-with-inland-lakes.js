c829b4b089be832d56177aa0047c0ebc2d788366
peter Sobhy <petersobhy@gmail.com>
2021-11-19 19:12:49 +0200

    island with inladn lake

--- bigger-island-with-inland-lakes.js
+++ bigger-island-with-inland-lakes.js
@@ -3,26 +3,28 @@ function recursiveIslandsAndLakes(matrix) {
   var cols = matrix[0].length;
   var islands = [];
 
+  var visited = [];
+
   for (var i = 0; i < rows; i++) {
     for (var j = 0; j < cols; j++) {
-      if (matrix[i][j] == 1) {
+      markOpenWater(matrix, visited, i, j, rows, cols);
+    }
+  }
+
+  for (var i = 0; i < rows; i++) {
+    for (var j = 0; j < cols; j++) {
+      if (matrix[i][j] == 1 || matrix[i][j] == 0) {
         var island = getIsland(matrix, i, j, rows, cols);
         islands.push(island);
       }
     }
   }
 
-  // add any inlland lakes
-  for (var i = 0; i < islands.length; i++) {
-    var island = islands[i];
-    // loop
-  }
-
   return islands;
 }
 
 function getIsland(matrix, x, y, rows, cols) {
-  if (x < 0 || x >= rows || y < 0 || y >= cols || matrix[x][y] != 1) return []; // water or part of island
+  if (x < 0 || x >= rows || y < 0 || y >= cols || matrix[x][y] != 8) return []; // water or part of island
 
   matrix[x][y] = 0; // or 0 to exclude it on future calls
 
@@ -32,6 +34,27 @@ function getIsland(matrix, x, y, rows, cols) {
   var bottom = getIsland(matrix, x, y - 1, rows, cols); // bottom
 
   return [[x, y], ...right, ...left, ...top, ...bottom];
+}
+
+function markOpenWater(matrix, visited, x, y, rows, cols) {
+  if (
+    x < 0 ||
+    x >= rows ||
+    y < 0 ||
+    y >= cols ||
+    matrix[x][y] != 0 ||
+    visited[x + "," + y]
+  )
+    return;
+
+  visited[x + "," + y] = true;
+
+  if (x == 0 || x == rows - 1 || y == 0 || y == cols - 1) matrix[x][y] = 8;
+
+  markOpenWater(matrix, visited, x + 1, y, rows, cols);
+  markOpenWater(matrix, visited, x - 1, y, rows, cols);
+  markOpenWater(matrix, visited, x, y + 1, rows, cols);
+  markOpenWater(matrix, visited, x, y - 1, rows, cols);
 }
 
 // console.log(
